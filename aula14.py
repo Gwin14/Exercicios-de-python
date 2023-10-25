ed70c409e07372d2efb955eb0d19e60fde196e4b
Gwin14 <fabiospinheira@gmail.com>
2023-10-24 21:00:00 -0300

    aula 15 e mais
    correção da aula 14, exercicio da aula 15 e data.json

--- aula14.py
+++ aula14.py
@@ -1,1 +1,28 @@
 # Criar um programa que pergunta pro usuário o nome de um livro, o autor e o ano de lancamento até o usuario não querer mais. Em um dicionário. No final dizer os livros.
+dados = []
+while True:
+    escolha = input("Digite 1 para adicionar uma pessoa, 0 para sair: ")
+
+    if escolha == "0":
+        break
+
+    if escolha == "1":
+        quantidade = input("Quantos livros você deseja informar dessa pessoa: ")
+        dados_livros = []
+
+        for x in range(0, quantidade):
+            nome_livro = input("Qual o nome do livro: ")
+            ano_lancamento = int(input("Qual o ano de lançamento: "))
+            autor = input("Qual o autor do livro: ")
+
+        dados_livros.append(
+            {
+                "autor": autor,
+                "nome": nome_livro,
+                "ano": ano_lancamento,
+            }
+        )
+
+        dados.append({"livros": dados_livros})
+
+print(dados)
---
+++ aula15.py
@@ -0,0 +1,5 @@
+import json
+
+eu = json.load(open("data.json", "r"))
+
+print(eu)
